dea0228169a561db849b26aec5bdeb9d5fdf8866
wowplus <wowplus2@gmail.com>
2016-11-08 13:43:59 +0900

    update comment

--- seq/seq.go
+++ seq/seq.go
@@ -1,5 +1,7 @@
+// Package seq implements functions for well-known sequences like Fibonacci.
 package seq
 
+// Fib returns nth (from 0th) Fibonacci number.
 func Fib(n int) int {
 	p, q := 0, 1
 	for i := 0; i < n; i++ {
