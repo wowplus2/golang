3d0d63beee3465b54c22bddbd253633c593c9662
wowplus <wowplus2@gmail.com>
2016-11-18 17:57:39 +0900

    go slice sample update

--- samples02/sample-slice/sample-slice.go
+++ samples02/sample-slice/sample-slice.go
@@ -29,12 +29,58 @@ func slice_way3() {
 	fmt.Println("\tlen =", len(s), ", cap =", cap(s))	// len 0, cap 0
 }
 
-func sub_slice1() {
+func sub_slice() {
 	s := []int{0,1,2,3,4,5}
 	s = s[2:5]
 
-	fmt.Println("sub_slice1: ")
+	fmt.Println("sub_slice: ")
 	fmt.Println("\t", s)	// 2,3,4 출력
+}
+
+func append_slice() {
+	s := []int{0, 1}
+	// 하나의 요소 확장
+	s = append(s, 2)	// 0, 1, 2
+	// 여러개 요소 확장
+	s = append(s, 3, 4, 5)	// 0, 1, 2, 3, 4, 5
+
+	fmt.Println("append_slice: ")
+	fmt.Println("\t", s)
+}
+
+func append_extend_slice1() {
+	// len = 0, cap = 3 인 slice
+	sliceA := make([]int, 0, 3)
+	// 계속 한 요소씩 추가한다.
+	fmt.Println("append_extend_slice1: ")
+	for i := 1; i <= 15; i++ {
+		sliceA = append(sliceA, i)
+		// slice 길이와 용량 확인
+		fmt.Println("\tlen =",len(sliceA),", cap =",cap(sliceA))
+	}
+	fmt.Println(sliceA)
+}
+
+func append_extend_slice2() {
+	sliceA := []int{1,2,3}
+	sliceB := []int{4,5,6}
+
+	sliceA = append(sliceA, sliceB...)
+	// sliceA = append(sliceA, 4,5,6)
+
+	fmt.Println("append_extend_slice2: ")
+	fmt.Println("\t",sliceA)
+}
+
+func copy_slice() {
+	src := []int{0,1,2}
+	trg := make([]int, len(src), cap(src)*2)
+
+	copy(trg, src)
+
+	fmt.Println("copy_slice: ")
+	fmt.Println("\t",trg)	// [0 1 2]
+	fmt.Println("\tlen =",len(trg),", cap =",cap(trg))
 }
 
 
@@ -45,5 +91,13 @@ func main() {
 	fmt.Println()
 	slice_way3()
 	fmt.Println()
-	sub_slice1()
+	sub_slice()
+	fmt.Println()
+	append_slice()
+	fmt.Println()
+	append_extend_slice1()
+	fmt.Println()
+	append_extend_slice2()
+	fmt.Println()
+	copy_slice()
 }
