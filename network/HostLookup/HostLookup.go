809999513cfad0c515db698787a292d0069f25b5
wowplus2 <wowplus2@gmail.com>
2016-12-30 02:20:47 +0900

    go network samples

---
+++ network/HostLookup/HostLookup.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s hostname\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	addrs, err := net.LookupHost(name)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		os.Exit(2)
+	}
+
+	for v, s := range addrs {
+		fmt.Println(v+1, "=>", s)
+	}
+	os.Exit(0)
+}
